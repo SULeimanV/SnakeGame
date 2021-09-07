1fe5e9149a6b30f5e0891001a9704453130c146a
SULeimanV <88591086+SULeimanV@users.noreply.github.com>
2021-09-07 21:18:11 +0300

    Update Snake.java

--- core/src/ru/xsph/f0482080/snakegame/Snake.java
+++ core/src/ru/xsph/f0482080/snakegame/Snake.java
@@ -17,7 +17,7 @@ public class Snake extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 1, 1);
+		ScreenUtils.clear(1, 0, 0.56f, 1);
 		batch.begin();
 		batch.draw(img, 1, 3);
 		batch.end();
