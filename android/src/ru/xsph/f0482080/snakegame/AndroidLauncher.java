dea7ac36b748109d77766158959ea38413d3a9f0
SULeimanV <molkp35@gmail.com>
2021-09-07 20:52:53 +0300

    add git repo

---
+++ android/src/ru/xsph/f0482080/snakegame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.xsph.f0482080.snakegame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.xsph.f0482080.snakegame.Snake;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Snake(), config);
+	}
+}
