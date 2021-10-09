e2d5ae173c0abee34785ac6a09e78da315a11434
SULeimanV <88591086+SULeimanV@users.noreply.github.com>
2021-10-09 22:30:53 +0300

    Update Snake.java
    obnova

--- core/src/ru/xsph/f0482080/snakegame/Snake.java
+++ core/src/ru/xsph/f0482080/snakegame/Snake.java
@@ -12,6 +12,9 @@ public class Snake extends ApplicationAdapter {
 	Texture snakeback;
 	Texture normalfood;
 	Texture badfood;
+	Rectangle bucket;
+	Sound eatn;
+	Sound eatb;
 	#tttthj
 
 	
@@ -31,10 +34,20 @@ public class Snake extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		//img = new Texture("badlogic.jpg");
 		
+		eatn = Gdx.audio.newSound(Gdx.files.internal("eatn.wav"));
+		eatb = Gdx.audio.newSound(Gdx.files.internal("eatb.wav"));
 		snakehead = new Texture("snake-hat.png");
 		snakeback = new Texture("snake-back.png");
 		normalfood = new Texture("food.png");
 		badfood = new Texture("bad-food.png");
+		
+		/*
+		bucket = new Rectangle();
+      		bucket.x = 800 / 2 - 64 / 2;
+      		bucket.y = 20;
+     		bucket.width = 64;
+      		bucket.height = 64;
+		*/
 	}
 
 	@Override
@@ -42,7 +55,8 @@ public class Snake extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0.56f, 1);
 		batch.begin();
 		//batch.draw(img, 1, 3);
-		batch.draw(snakehead, 1, 3);
+		batch.draw(snakehead, 0, 0);
+		batch.draw(snakehead, 10, 0);
 		batch.end();
 	}
 	
