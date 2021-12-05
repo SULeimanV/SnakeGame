007f058ebab237c4bcd1773aed3123f7a704ec4c
SULeimanV <molkp35@gmail.com>
2021-12-05 21:39:50 +0300

    imported alllllllkkhghg

--- core/src/ru/xsph/f0482080/snakegame/Snake.java
+++ core/src/ru/xsph/f0482080/snakegame/Snake.java
@@ -1,29 +1,60 @@
 package ru.xsph.f0482080.snakegame;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Snake extends ApplicationAdapter {
-	OrthographicCamera camera;
+public class Snake extends Game {
+	SpriteBatch batch;
+	BitmapFont font;
+	/*OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture snakehead;
 	Texture snakeback;
 	Texture normalfood;
 	Texture badfood;
 	Rectangle bucket;
-	Sound eatn;
-	Sound eatb;
-	//tttthj
+	int x = 0;
+	int y = 0;
+	//Sound eatn;
+	//Sound eatb;
+	//tttthj*/
 
 	
 	@Override
 	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		this.setScreen(new StartMenu(this));
+		/*Gdx.input.setInputProcessor(new SimpleDirectionGestureDetector(new SimpleDirectionGestureDetector.DirectionListener() {
+
+		@Override
+		public void onUp() {
+		//System.out.println("f");
+			y+=10;
+		}
+
+		@Override
+		public void onRight() {
+			x+=10;
+		}
+
+		@Override
+		public void onLeft() {
+			x-=10;
+		}
+
+		@Override
+		public void onDown() {
+			y-=10;
+		}
+		}));
+
+
+		//swipelistener new
+		//Gdx.input.setInputProcessor(new swipelistener());
+
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 480, 800);
 		
@@ -38,8 +69,8 @@ public class Snake extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		//img = new Texture("badlogic.jpg");
 		
-		eatn = Gdx.audio.newSound(Gdx.files.internal("eatn.wav"));
-		eatb = Gdx.audio.newSound(Gdx.files.internal("eatb.wav"));
+		//eatn = Gdx.audio.newSound(Gdx.files.internal("eatn.wav"));
+		//eatb = Gdx.audio.newSound(Gdx.files.internal("eatb.wav"));
 		snakehead = new Texture("snake-hat.png");
 		snakeback = new Texture("snake-back.png");
 		normalfood = new Texture("food.png");
@@ -56,17 +87,22 @@ public class Snake extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0.56f, 1);
+		super.render();
+		/*ScreenUtils.clear(1, 0, 0.56f, 1);
 		batch.begin();
 		//batch.draw(img, 1, 3);
-		batch.draw(snakehead, 0, 0);
+		batch.draw(snakehead, x, y);
 		batch.draw(snakehead, 10, 0);
-		batch.end();
+
+		batch.end();*/
 	}
 	
 	@Override
 	public void dispose () {
+		super.dispose();
 		batch.dispose();
+		font.dispose();
+		/*batch.dispose();*/
 		//img.dispose();
 	}
 }
